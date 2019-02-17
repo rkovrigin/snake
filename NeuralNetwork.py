ebc813721bcf67dcb51041f6c624855db9a85b3c
Roman Kovrigin <romakovrigin@gmail.com>
2019-02-17 19:58:49 +0100

    Finish a simple 1 layer NN

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -11,9 +11,10 @@ def sigmoid_gradient(np_array):
 
 class NeuralNetwork(object):
 
-    def __init__(self, output_layer_size=None, internal_layers=1):
-        statistic = Statistic(output="dump_nn.txt")
+    def __init__(self, output_layer_size=None, internal_layers=1, my_lambda=0):
+        statistic = Statistic(output="dump.txt")
         self.X, self.Y, self.m = statistic.get_training_set()
+        self.my_lambda = my_lambda
 
         self.input_layer_size = self.X.shape[1]
         if not output_layer_size:
@@ -25,11 +26,16 @@ class NeuralNetwork(object):
         else:
             self.internal_layers = internal_layers
 
-        self.theta1 = np.zeros((self.internal_layer_size, self.input_layer_size + 1))
-        self.theta2 = np.zeros((self.output_layer_size, self.internal_layer_size + 1))
+        # self.theta1 = np.random.rand((self.internal_layer_size, self.input_layer_size + 1))
+        # self.theta2 = np.random.rand((self.output_layer_size, self.internal_layer_size + 1))
+        self.theta1 = np.random.rand(self.internal_layer_size, self.input_layer_size + 1)
+        self.theta2 = np.random.rand(self.output_layer_size, self.internal_layer_size + 1)
 
-
-    def init_weights(self):
+    def randomize_weights(self):
+        """
+        epsilon_init = 0.12;
+        W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
+        """
         pass
 
     """
@@ -60,14 +66,16 @@ class NeuralNetwork(object):
         theta1_grad = np.zeros(self.theta1.shape)
         theta2_grad = np.zeros(self.theta2.shape)
 
-        for i in range(2):
-            n = self.X[0].shape[0]
-            a1 = np.hstack((np.ones(1), self.X[1])).reshape(1, n+1)
-            z2 = np.matmul(a1, np.transpose(self.theta1))
-            a2 = sigmoid(z2)
-            a2 = np.transpose(np.vstack(np.hstack((np.ones(1), a2[0]))))
-            z3 = np.matmul(a2, np.transpose(self.theta2))
-            a3 = sigmoid(z3)
+        for i in range(self.m):
+            # n = self.X[0].shape[0]
+            # a1 = np.hstack((np.ones(1), self.X[i])).reshape(1, n+1)
+            # z2 = np.matmul(a1, np.transpose(self.theta1))
+            # a2 = sigmoid(z2)
+            # a2 = np.transpose(np.vstack(np.hstack((np.ones(1), a2[0]))))
+            # z3 = np.matmul(a2, np.transpose(self.theta2))
+            # a3 = sigmoid(z3)
+
+            a1, a2, a3, z2, z3 = self._calc_inner_layers(self.X[i])
 
             d3 = a3 - self.Y[i]
             d2 = np.matmul(d3, self.theta2[:, 1:]) * sigmoid_gradient(z2)
@@ -75,9 +83,38 @@ class NeuralNetwork(object):
             theta1_grad = theta1_grad + np.matmul(np.transpose(d2), a1)
             theta2_grad = theta2_grad + np.matmul(np.transpose(d3), a2)
 
+        t1_tmp = theta1_grad.copy()
+        t1_tmp[:, t1_tmp.shape[1] - 1] = np.ones(t1_tmp.shape[0])
+        theta1_grad = theta1_grad + (self.my_lambda * t1_tmp)/self.m
+
+        t2_tmp = theta2_grad.copy()
+        t2_tmp[:, t2_tmp.shape[1] - 1] = np.ones(t2_tmp.shape[0])
+        theta2_grad = theta2_grad + (self.my_lambda * t2_tmp)/self.m
+
+        return theta1_grad, theta2_grad
+
+    def _calc_inner_layers(self, x):
+        n = x.shape[0]
+        a1 = np.hstack((np.ones(1), x)).reshape(1, n + 1)
+        z2 = np.matmul(a1, np.transpose(self.theta1))
+        a2 = sigmoid(z2)
+        a2 = np.transpose(np.vstack(np.hstack((np.ones(1), a2[0]))))
+        z3 = np.matmul(a2, np.transpose(self.theta2))
+        a3 = sigmoid(z3)
+        return a1, a2, a3, z2, z3
+
+    def predict(self, layer_1_input):
+        a1, a2, a3, z2, z3 = self._calc_inner_layers(layer_1_input)
+        return a3
+
 def main():
-    nn = NeuralNetwork()
-    nn.learning()
+    nn = NeuralNetwork(my_lambda=3)
+    t1, t2 = nn.learning()
+    print(t1)
+    print(t2)
+
+    a = nn.predict(nn.X[0])
+    print(a)
 
 
 if __name__ == "__main__":
